a471824a75e243872cbbec8cafa941862ed24c16
gaurav rattan <gauravrattan76@gmail.com>
2019-08-03 23:10:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var app = require('./app');
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+server.listen(port,()=>{
+  console.log("app has started");
+});
