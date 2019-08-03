afec1f1bb453421b80e7b62a470156e3f82684c2
gaurav rattan <gauravrattan76@gmail.com>
2019-08-04 01:11:58 +0530

    third commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
+app.use(morgan('dev'));
+//defined routes
 var orderRoutes = require('./api/routes/orders');
 var productsRoutes = require('./api/routes/products');
 
 app.use('/orders',orderRoutes);
 app.use('/products',productsRoutes);
-app.use((req,res) =>{
-    res.status(200).json({
-        message:"Welcome to the Rest api"
+
+
+app.use((req,res,next) =>{
+    var error = new Error("Not Found");
+     error.status = 404;
+     next(error);
+});
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 404);
+    res.json({
+        error:{
+            message:error.message
+        }
     })
 });
 
