9f15ea6ac482f48371425472257200871aabb716
gaurav rattan <gauravrattan76@gmail.com>
2019-08-04 00:57:44 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+var orderRoutes = require('./api/routes/orders');
+var productsRoutes = require('./api/routes/products');
+
+app.use('/orders',orderRoutes);
+app.use('/products',productsRoutes);
 app.use((req,res) =>{
     res.status(200).json({
         message:"Welcome to the Rest api"
